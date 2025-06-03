88bb1abbb804e2d39806e05c217ba9d43fd758b9
saudk <saudk1539@gmail.com>
2025-06-03 15:01:59 +0500

    Updated contact form and EmailJS integration3

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/portfolio/' : '/',
+export default defineConfig({
+  base: './',
   plugins: [react()],
-}))
+})
